fb28cc98b828a4cfbf33e2fc20cdd628106195db
My Laptop <abdelselouane@gmail.com>
2016-03-30 19:16:12 -0400

    zooming

--- assets/js/LandingPage.js
+++ assets/js/LandingPage.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
         el: '#map',
         lat: schoolLat,
         lng: schoolLon,
-        zoom: 13
+        zoom: 5
     });
     
     $.ajax({
@@ -34,6 +34,7 @@ $(document).ready(function(){
                     }
                 }
             }
+            console.log(ridesMap);
             map.addMarkers(ridesMap);
         },
         error: function(jqXHR, textStatus, errorThrown) {
